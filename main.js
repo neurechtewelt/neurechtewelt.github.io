0f33b32d4b0ee0621998ce7701a15b37f0def17a
neurechtewelt <neurechtewelt@posteo.de>
2017-01-06 10:57:38 +0000

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function updatePage() {
-  var target = selectedValue("page") + "-" + selectedValue("week") + "-" +selectedValue("linksource") + ".html"
-  window.location.href = target;
-}
-
-function selectedValue(elemId) {
-  var e = document.getElementById(elemId);
-  return e.options[e.selectedIndex].value;
-}
